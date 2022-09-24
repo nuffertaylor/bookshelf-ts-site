16841ca79ff38141f7a07b45a910d03a0ce22671
Taylor Nuffer <nuffertaylor@gmail.com>
2022-09-23 23:35:54 -0600

    made leaderboard dynamic

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,45 +1,49 @@
 import React from 'react';
 import { defaultProps } from '../types/interfaces';
+import { getCookie, sendGetRequestToServer } from '../utilities';
+import { Loading } from './Loading';
+
+interface rank{
+  username : string,
+  spines : number
+}
+interface leaderboard_res{
+  statusCode : number,
+  body : Array<rank>
+}
 
 export function Leaderboard({widgetCallback} : defaultProps){
-  return(
-  <div className="leaderboard_box">
-    <div className="leaderboard_row title_row">
-      <span className="leaderboard_rank">rank</span>
-      <span className="leaderboard_name">username</span>
-      <span className="leaderboard_score">spines</span>
-    </div>
-    <div className="bs_box_line"></div>
-    <div className="leaderboard_row">
-      <span className="leaderboard_rank">1</span>
-      <span className="leaderboard_name">jdoe</span>
-      <span className="leaderboard_score">25</span>
-    </div>
-    <div className="bs_box_line"></div>
-    <div className="leaderboard_row">
-      <span className="leaderboard_rank">2</span>
-      <span className="leaderboard_name">bsmith</span>
-      <span className="leaderboard_score">23</span>
-    </div>
-    <div className="bs_box_line"></div>
-        <div className="leaderboard_row">
-      <span className="leaderboard_rank">3</span>
-      <span className="leaderboard_name">alfred</span>
-      <span className="leaderboard_score">21</span>
-    </div>
-    <div className="bs_box_line"></div>
-        <div className="leaderboard_row">
-      <span className="leaderboard_rank">...</span>
-      <span className="leaderboard_name">...</span>
-      <span className="leaderboard_score">...</span>
-    </div>
-    <div className="bs_box_line"></div>
-    <div className="leaderboard_row title_row">
-      <span className="leaderboard_rank">101</span>
-      <span className="leaderboard_name">you</span>
-      <span className="leaderboard_score">3</span>
-    </div>
-    <div className="bs_box_line"></div>
-  </div>
-  );
+  const cur_user = getCookie("username");
+  sendGetRequestToServer("leaderboard", "", (res : string)=>{
+    const parsed_res : leaderboard_res = JSON.parse(res);
+    const ranks = parsed_res.body.map((v, i) => {
+      let classes = "leaderboard_row"
+      if(v.username === cur_user) classes += " title_row";
+      return(
+        <div>
+          <div className={classes} key={v.username + i.toString()}>
+            <span className="leaderboard_rank">{i+1}</span>
+            <span className="leaderboard_name">{v.username}</span>
+            <span className="leaderboard_score">{v.spines}</span>
+          </div>
+          <div className="bs_box_line"></div>
+        </div>
+      );
+    });
+    const leaderboard = (
+      <div className="leaderboard_box">
+        <div>
+          <div className="leaderboard_row title_row">
+            <span className="leaderboard_rank">rank</span>
+            <span className="leaderboard_name">username</span>
+            <span className="leaderboard_score">spines</span>
+          </div>
+          <div className="bs_box_line"></div>
+        </div>
+        {ranks}
+      </div>
+    );
+    widgetCallback(leaderboard);
+  });
+  return <Loading/>
 }
