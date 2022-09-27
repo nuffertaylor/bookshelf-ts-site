816a97ead1df4024910aeeeeb6003aaf4934914e
Taylor Nuffer <nuffertaylor@gmail.com>
2022-09-26 20:16:14 -0600

    refactored to use sort page before generating shelf

--- src/pages/Found.tsx
+++ src/pages/Found.tsx
@@ -1,54 +1,27 @@
 import React from 'react';
 import {Loading} from "./Loading";
 import { UnfoundUpload } from './UnfoundUpload';
-import { YourShelf } from './YourShelf';
 import { book, bookContainer, foundBook } from '../types/interfaces';
+import { SortBy } from './SortBy';
 
 export interface FoundProps {
   found : Array<foundBook>, 
   unfound ?: Array<bookContainer>,
   widgetCallback : Function
 }
-interface genshelfRequest {
-  bookList : Array<Object>
-}
-interface genshelfResponse {
-  statusCode : number,
-  body : string
-}
 
 export function Found({found, unfound, widgetCallback} : FoundProps){
-  const sendPostRequestToServer = async function (method : string, data : genshelfRequest, callback : Function) {
-    var httpPost = new XMLHttpRequest();
-    var path : string = "https://vi64h2xk34.execute-api.us-east-1.amazonaws.com/alpha/" + method;
-    var data_json : string = JSON.stringify(data);
-    httpPost.onreadystatechange = (err) =>{
-      if(httpPost.readyState === 4) callback(httpPost.responseText);
-    };
-    // Set the content type of the request to json since that's what's being sent
-    httpPost.open("POST", path, true);
-    httpPost.setRequestHeader('Content-Type', 'application/json');
-    httpPost.send(data_json);
-  };
-
-  const go = ()=>{
-    widgetCallback(<Loading/>);
-    let booklist : Array<book> = found;
-      if((document.getElementById("fakeSpines") as HTMLInputElement)?.checked && unfound) {
-        unfound.forEach(u=>booklist.push(u.book));
-      }
-      var data = {
-        bookList : booklist
-      }
-      sendPostRequestToServer("genshelf", data, (res : string)=>{
-        const resObj : genshelfResponse = JSON.parse(res);
-        const url : string = resObj.body as string;
-        widgetCallback(<YourShelf shelf_url={url}/>)
-      });
-  };
-
   const createUnfoundUpload =()=>{
     widgetCallback(<UnfoundUpload found={found} unfound={unfound} widgetCallback={widgetCallback}/>);
+  }
+
+  const openSortBy = ()=>{
+    widgetCallback(<Loading/>);
+    let booklist : Array<book> = found;
+    if((document.getElementById("fakeSpines") as HTMLInputElement)?.checked && unfound) {
+      unfound.forEach(u=>booklist.push(u.book));
+    }
+    widgetCallback(<SortBy widgetCallback={widgetCallback} booklist={booklist}/>);
   }
 
   return(
@@ -57,12 +30,13 @@ export function Found({found, unfound, widgetCallback} : FoundProps){
       {unfound && unfound.length > 0 &&
       <div>
         <div className="unfound_spine_subhead">Missing {unfound.length} Spines</div>
+        {/* comment in when fake spine generator fixed
         <input type="checkbox" id="generate_fakes"/>
-        <label htmlFor="generate_fakes">generate fake spines</label>
+        <label htmlFor="generate_fakes">generate fake spines</label> */}
       </div>
       }
       <div className="bs_button_wrapper">
-        <button className="bs_adaptive_button" onClick={go}>Generate</button>
+        <button className="bs_adaptive_button" onClick={openSortBy}>Sort your Books</button>
       </div>
       {unfound && unfound.length > 0 &&
       <div className="bs_button_wrapper">
--- src/pages/SortBy.tsx
+++ src/pages/SortBy.tsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { MouseEventHandler, useState } from 'react';
+import { book, defaultProps } from '../types/interfaces';
+import { Select } from '@mantine/core';
+import { sendPostRequestToServer } from '../utilities';
+import { Loading } from './Loading';
+import { YourShelf } from './YourShelf';
 
-export function SortBy(){
+interface sortByProps extends defaultProps{
+  booklist : Array<book>
+}
+interface genshelfRequest {
+  bookList : Array<Object>
+}
+interface genshelfResponse {
+  statusCode : number,
+  body : string
+}
+
+export function SortBy({widgetCallback, booklist} : sortByProps){
+  const [selectValue, setSelectValue] = useState<string | null>(null);
+  const [buttonText, setButtonText] = useState<string>("Generate Shelf");
+  const [disabledClass, setDisabledClass] = useState<string>("bs_disabled");
+
+  const submit_main_click = ()=>{
+    if(selectValue === null) return;
+    //TODO: Add sort method calls here before calling generateShelf for each type
+    generateShelf();
+  }
+
+  const generateShelf = ()=>{
+    widgetCallback(<Loading/>);
+    const data = {
+      bookList : booklist
+    }
+    sendPostRequestToServer("genshelf", data, (res : string)=>{
+      const resObj : genshelfResponse = JSON.parse(res);
+      const url : string = resObj.body as string;
+      widgetCallback(<YourShelf shelf_url={url}/>)
+    });
+  }
+
+  const alterSelectValue = (val:string)=>{
+    if(disabledClass) setDisabledClass("");
+    setSelectValue(val);
+    if(val === "Sort Manually") setButtonText("Start Manual Sort");
+    else setButtonText("Generate Shelf");
+  }
+
   return(
   <div className="found_spine_box">
     <div className="found_spine_head">Sort by</div>
     <div className="custom-select" style={{width:"200px"}}>
-      <select>
-        <option value="0">Select option:</option>
-      </select>
+    <Select
+        data={['Default Sort', 'Title', 'Year', 'Date Read', 'Author', 'Color', 'User Rating', 'Average Rating', 'Sort Manually']}
+        placeholder="Select Sort Method"
+        value={selectValue}
+        onChange={alterSelectValue}
+      />
     </div>
     <div className="bs_button_wrapper">
-      <button className="bs_adaptive_button">Go</button>
+      <button className={disabledClass + " bs_button"} id="sort_main_button" onClick={submit_main_click}>{buttonText}</button>
     </div>
   </div>
   )
