a130d62210a92a1f91584ab3ffdc8cfa303c6a5f
Taylor Nuffer <nuffertaylor@gmail.com>
2022-10-23 12:12:12 -0600

    added footer file

---
+++ src/FooterCentered.tsx
@@ -0,0 +1,56 @@
+import { createStyles, Anchor, Group } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 50,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: `${theme.spacing.md}px ${theme.spacing.md}px`,
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.lg,
+      marginBottom: theme.spacing.sm,
+    },
+  },
+}));
+
+interface FooterCenteredProps {
+  links: { link: string; label: string }[];
+}
+
+export function FooterCentered({ links }: FooterCenteredProps) {
+  const { classes } = useStyles();
+  const items = links.map((link) => (
+    <Anchor<'a'>
+      color="dimmed"
+      key={link.label}
+      href={link.link}
+      sx={{ lineHeight: 1 }}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <div className={classes.inner}>
+        <Group className={classes.links}>{items}</Group>
+      </div>
+    </div>
+  );
+}
