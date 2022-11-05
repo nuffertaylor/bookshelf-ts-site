ee615071e720932a4045cfd12316a34dd07de82c
Taylor Nuffer <nuffertaylor@gmail.com>
2022-11-05 17:06:15 -0600

    added directions for editing book spine images

--- src/pages/HowTo.tsx
+++ src/pages/HowTo.tsx
@@ -80,7 +80,46 @@ export function HowTo(){
     {
       value: "edit_spines",
       control: "How do you edit spine images?",
-      panel: ""
+      panel: (
+        <div>
+				  <p>Unfortunately, getting spine images we can actually use requires a little bit of editing. Feel free to use an image editor of your choice, but I use GIMP, it’s a great tool, and you can <a className={"a_".concat(colorScheme)} href="https://www.gimp.org/downloads/">download it for free here.</a></p>
+          <ol>
+            <li>Snap a picture of the spine you want to upload. Try and get it as centered and straight as possible, as it makes editing easier.
+              <img className="bs_faq_img" src="https://i.imgur.com/69C4ThD.jpg" alt="original unedited"/>
+            </li>
+            <li>Open the image in GIMP (either right click and open with, or drag and drop the image into GIMP)</li>
+            <li>Press “F” and click on the four corners of the book spine until you’ve made a rectangle.
+              <img className="bs_faq_img" src="https://i.imgur.com/HA4k1RN.gif" alt="select book spine corners in gimp"/>
+            </li>
+            <li>Press “ctrl+x”, “ctrl+v” to cut the selected spine and repaste it.</li>
+            <li>In the right hand corner of GIMP, click the leftmost icon to paste the spine onto a new layer.
+              <img className="bs_faq_img" src="https://i.imgur.com/eZ9REcC.jpg" alt="paste new layer"/>
+            </li>
+            <li>You should now see two layers on the bottom right section, one containing your spine, and one containing the background. Right click on the background one and click “Delete Layer”
+              <img className="bs_faq_img" src="https://i.imgur.com/bw4JnKq.png" alt="delete background layer"/>
+            </li>
+            <li>Now your spine is swimming in a sea of opacity. Go to the menu bar, click “Image”, then click “Fit Canvas to Layers.”
+              <img className="bs_faq_img" src="https://i.imgur.com/CEMLAyz.png" alt="fit canvas to layers"/>
+            </li>
+            <li>It should be looking pretty good now, but we aren’t quite done. If you zoom in on the image (press “Z” and click on the area you want to zoom in on), you’ll notice the book spine isn’t completely rectangular. This is a perspective issue and we can fix it!</li>
+            <li>Zoom in on each corner of the image. If you notice opacity there (shown by a grid), adjust the perspective. To do this, press “Shift+P” to open the perspective tool. Now you can drag the corner to adjust the image until it looks right. Repeat this for every corner/edge until there’s no opacity, and the spine looks straight. (Note: Just like zooming in, you can zoom out by pressing the minus key “-” and clicking anywhere)
+              <img className="bs_faq_img" src="https://i.imgur.com/4JiTdde.gif" alt="adjust perspective"/>
+            </li>
+            <li>And we’re almost done! Now we need to export the image. Press “ctrl+shift+e” to open the export menu.</li>
+            <li>From here, type in any name for your image (usually I name it the title of the book), and change the file extension to be .jpg. Doing this will ensure the file is compressed to a reasonable size when used on the site. You can also do png, but it might be too big (the my bookshelf site accepts a max filesize of 5mb). You have to do jpg/png though, the site will reject other file types, like HEIC. Once you’ve done this, click export.
+              <img className="bs_faq_img" src="https://i.imgur.com/OGnZuON.png" alt="export your edit"/>
+            </li>
+            <li>Another menu will pop up, and here you can adjust some of the metadata or quality of the image. Normally you can just leave it at the default “90,” but if you’re struggling to make your image file size small enough, you can set that number to be smaller. Click “Export” again, and we’re done!
+              <img className="bs_faq_img" src="https://i.imgur.com/QxnPF1q.png" alt="export settings"/>
+            </li>
+            <li>You now have a spine image ready to upload to MyBookshelf!</li>
+          </ol>
+         <div style={{display: "flex", justifyContent: "center"}}>
+          <img className="bs_faq_img" src="https://i.imgur.com/ngys0Ib.jpg" style={{width:"50px"}} alt="finished_spine"/>
+        </div>
+
+			</div>
+      )
     },
     {
       value: "find_dimensions",
