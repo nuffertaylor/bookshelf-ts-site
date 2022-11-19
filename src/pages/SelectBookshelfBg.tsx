2d91ad1b3e7334f11c90d28ae79e20a01e72e87c
Taylor Nuffer <nuffertaylor@gmail.com>
2022-11-18 23:52:06 -0700

    added interfaces for selectbookshelfbg

--- src/pages/SelectBookshelfBg.tsx
+++ src/pages/SelectBookshelfBg.tsx
@@ -2,20 +2,33 @@ import { Select } from '@mantine/core';
 import React, { useState } from 'react';
 import { defaultProps } from "../types/interfaces";
 
-export function SelectBookshelfBg({widgetCallback} : defaultProps){
+export interface shelf_bg{
+    bg_id : string,
+    submitter : string,
+    filename : string,
+    width_inches : number,
+    width_pixels : number,
+    shelf_bottoms : number[],
+    shelf_left : number,
+    timestamp : number,
+    title : string
+}
+interface selectBookshelfProps extends defaultProps {
+  shelf_bgs : shelf_bg[]
+}
+
+export function SelectBookshelfBg({widgetCallback, shelf_bgs} : selectBookshelfProps){
   const [selectValue, setSelectValue] = useState<string | null>(null);
   const alterSelectValue = (val:string)=>{
     setSelectValue(val);
   }
-  const [selectData, setSelectData] = useState<string[]>([
-    "default1.jpg"
-  ])
+  const [selectData, setSelectData] = useState<shelf_bg[]>(shelf_bgs);
 
   return(
     <div>
       <div className="custom-select" style={{width:"200px"}}>
         <Select
-            data={selectData}
+            data={selectData.map(d => d.title)}
             placeholder="Select Sort Method"
             value={selectValue}
             onChange={alterSelectValue}
