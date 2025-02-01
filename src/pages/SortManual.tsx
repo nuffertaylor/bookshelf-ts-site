1219a0434c2d1dd5ae52b0e8f39349ad656e48a9
incrediblejonas <nuffertaylor@gmail.com>
2025-02-01 13:04:23 -0700

    added customize button to sort manual page

--- src/pages/SortManual.tsx
+++ src/pages/SortManual.tsx
@@ -4,6 +4,7 @@ import { DragAndDropList } from './DragAndDropList';
 import { SortBy } from './SortBy';
 import { Title } from './Title';
 import { BookshelfRendererParams } from '../utils/BookshelfRenderer';
+import { CustomizeShelf } from './CustomizeShelf';
 
 export interface sortManualProps extends defaultProps{
   booklist : Array<foundBook>,
@@ -13,7 +14,9 @@ export function SortManual({widgetCallback, booklist, genShelf} : sortManualProp
   const returnToSortBy = ()=>{widgetCallback(<SortBy widgetCallback={widgetCallback} booklist={booklist}/>)};
   const submit_man_sort = ()=>{ genShelf({books: booklist}) };
   const update_booklist = (bl : foundBook[]) => { booklist = bl };
-
+  const customize_shelf_click = () => {
+    widgetCallback(<CustomizeShelf widgetCallback={widgetCallback} booklist={booklist} genShelf={genShelf}/>)
+  };
   //should I make the submit button sticky?
   return(
     <div>
@@ -21,6 +24,11 @@ export function SortManual({widgetCallback, booklist, genShelf} : sortManualProp
       <DragAndDropList data={booklist} updateParent={update_booklist}/>
       <div className="bs_button_wrapper">
         <button className="bs_button" id="submit_manual_sort" onClick={submit_man_sort}>Generate Shelf</button>
+      </div>
+      <div className="bs_button_wrapper">
+        <button className="bs_button" id="customize_shelf_button" onClick={customize_shelf_click}>
+          Customize Shelf
+        </button>
       </div>
     </div>
   )
